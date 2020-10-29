90b5923f6661c5149d0c6864fbd57e8445a8087f
Devan Pellow <pellod01@stepstone.com>
2020-10-29 19:46:51 -0400

    1 - add Chakra tooltip component to launch detail time

--- src/components/launch.js
+++ src/components/launch.js
@@ -13,6 +13,7 @@ import {
   SimpleGrid,
   Box,
   Text,
+  Tooltip,
   Spinner,
   Image,
   Link,
@@ -124,7 +125,9 @@ function TimeAndLocation({ launch }) {
           </Box>
         </StatLabel>
         <StatNumber fontSize={["md", "xl"]}>
-          {formatDateTime(launch.launch_date_local)}
+          <Tooltip hasArrow label={'Your local time: ' + formatDateTime(launch.launch_date_local)}>
+            {formatDateTime(launch.launch_date_local)}
+          </Tooltip>
         </StatNumber>
         <StatHelpText>{timeAgo(launch.launch_date_utc)}</StatHelpText>
       </Stat>
